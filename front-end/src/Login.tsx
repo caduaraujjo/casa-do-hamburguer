be0beeb68e2036e8ffc7efb293a732a773fd8501
caduaraujjo <caduaraujo1609@gmail.com>
2026-07-05 23:28:26 -0300

    feat: cria componentel de login

---
+++ front-end/src/Login.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Input from "./components/Input";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  }
+
+  return (
+    <form
+      className="flex h-screen items-center justify-center bg-[#161410]"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex flex-col items-center justify-center gap-2">
+        <img src="./logo.png" alt="" className="mb-4" />
+        <Input
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          type="Password"
+        />
+        <button className="w-full cursor-pointer rounded-md bg-[#C92A0E] py-2 text-sm font-bold text-white">
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
