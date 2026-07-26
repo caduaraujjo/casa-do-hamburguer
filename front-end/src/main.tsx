2c8d2fd053b386e8424c19553af3428db85ff9b4
caduaraujjo <caduaraujo1609@gmail.com>
2026-07-26 18:13:44 -0300

    feat: add login and register pages with routing

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Register from "./Register.tsx";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Register />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
